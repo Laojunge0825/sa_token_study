0c23f6994f265769d953cd50e4467b3909746669
pengcanjun <1642287765@qq.com>
2025-03-13 15:32:52 +0800

    初始化项目

---
+++ src/main/java/com/shuke/sa_token_study/SaTokenStudyApplication.java
@@ -0,0 +1,13 @@
+package com.shuke.sa_token_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaTokenStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaTokenStudyApplication.class, args);
+    }
+
+}
